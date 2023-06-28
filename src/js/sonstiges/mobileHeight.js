329b1fc63b998a1dc3443b2c2b536ec1573cac54
oezkan55 <alibeyoezkan@googlemail.com>
2023-06-28 10:46:04 +0200

    Add comment mobileHeight.js

--- src/js/sonstiges/mobileHeight.js
+++ src/js/sonstiges/mobileHeight.js
@@ -1,5 +1,8 @@
 "use strict";
 
+//? Problem beheben bei Mobile Ansicht mit 100vh Body!
+//? wegen Adressleiste vom Browser ensteht ein unschöner effekt
+
 const documentHeight = () => {
   const doc = document.documentElement;
   doc.style.setProperty("--doc-height", `${window.innerHeight}px`);
